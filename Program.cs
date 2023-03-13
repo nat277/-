9334379ae3681c7096ee70491c5e20177efcf655
«nat» <nat27.kozlowa@yandex.ru>
2023-03-13 13:51:43 +0400

    вывод результата на экран

--- Program.cs
+++ Program.cs
@@ -25,3 +25,14 @@ void SecondArrayWith(string[] array1, string[] array2)
         }
     }
 }
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWith(array1, array2);
+PrintArray(array2);
